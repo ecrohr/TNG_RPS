1605794afa3cbd06446dd8835502fba540a54f97
Eric Rohr <rohr@mpia.de>
2022-09-30 16:59:23 +0200

    Functionality for snaps > snapNum.

--- src/tng_rps/Create_TracerTracks.py
+++ src/tng_rps/Create_TracerTracks.py
@@ -52,10 +52,10 @@ def create_tracertracks():
 
     part_fields = ['ParticleIDs']
 
-    big_array_length = int(1e6)
+    big_array_length = int(1e8)
 
     # define the subhalos we care about at snapshot snapNum
-    subfindIDs = range(3)
+    subfindIDs = range(10000)
 
     outdirec = '../Output/%s_tracers_%d-%d/'%(sim,subfindIDs[0],subfindIDs[-1])
     print(outdirec)
@@ -69,7 +69,7 @@ def create_tracertracks():
     print('create_tracertracks: Initalization: %.2g s'%(b-a))
     
     # now track tracers from snapNum + 1 until snap 99
-    for snap in range(snapNum, snapNum+10):
+    for snap in range(snapNum, max_snap+1):
         match_tracers(snap)
 
     # and find the unmatched tracers from snapNum + 1 until snap 99
@@ -91,7 +91,7 @@ def match_tracers(snap):
     """
 
     print('Working on %s snap %03d'%(sim, snap))
-    
+       
     # load the subfindIDs
     with h5py.File(outdirec + 'offsets_%03d.hdf5'%snap, 'r') as f:
         subfindIDs = f['group']['SubfindID'][:]
@@ -123,14 +123,14 @@ def match_tracers(snap):
     # slice the the relevant arrays
     tracer_IDs = tracers['TracerID'][indices2]
     tracer_indices = indices2
-
+    
     particle_IDs = ParticleIDs[indices1]
     particle_indices = Particle_indices[indices1]
     temperatures = Temperatures[indices1]
     
     # for each subhalo, find the relevant particles + tracers and save
     for subfind_i, subfindID in enumerate(subfindIDs):
-
+        
         if subfind_i == 0:
             offsets_subhalo['SubhaloOffset'][subfind_i] = 0
         else:
@@ -143,13 +143,23 @@ def match_tracers(snap):
         if stop == start:
             continue
 
-        indices = ((indices1 >= start) & (indices1 < stop))
-        Ntracers = len(indices[indices])
+        sub_indices = ((indices1 >= start) & (indices1 < stop))
+        Ntracers = len(sub_indices[sub_indices])
         offsets_subhalo['SubhaloLength'][subfind_i]            = Ntracers
         offsets_subhalo['SubhaloLengthColdGas'][subfind_i]     = Ntracers
         offsets_subhalo['SubhaloLengthColdGas_new'][subfind_i] = Ntracers
         
+        tracer_IDs_sub = tracer_IDs[sub_indices]
+        tracer_indices_sub = tracer_indices[sub_indices]
+
         # if snap > SnapNum: cross match with previous snapshot(s)
+        """
+        This chunk needs to be rewritten using the new match3 scheme. Additionally, the 
+        group 3 tracer indices (and parentIDs) should be tabulated but only after finishing
+        the loop over subfindIDs. This means that we only do the match3 search on all tracers
+        at snap one time per snapshot rather than per subhalo. Or can keep this entirely separate and 
+        as part of the find_unmatched_tracers() routine.
+        """
         if snap > snapNum:
             ### check which cold gas cell tracers from previous snap are still here ###
 
@@ -157,6 +167,9 @@ def match_tracers(snap):
             if offsets_past['group']['SubfindID'][subfind_i] != -1:
                 start = offsets_past['group']['SubhaloOffset'][subfind_i]
                 end   = start + offsets_past['group']['SubhaloLengthColdGas'][subfind_i]
+                
+                IDs_past = tracers_past['group']['TracerIDs'][start:end]
+                indices_past = tracers_past['group']['TracerIndices'][start:end]
 
             # if not, then try the previous snapshots
             else:
@@ -170,54 +183,57 @@ def match_tracers(snap):
                             start = f['group']['SubhaloOffset'][subfind_i]
                             end   = start + f['group']['SubhaloLengthColdGas'][subfind_i]
                             f.close()
+                            with h5py.File(outdirec + 'tracers_%03d.hdf5'%(snap -i), 'r') as trace_past:
+                                IDs_past = trace_past['group']['TracerIDs'][start:end]
+                                indices_past = trace_past['group']['TracerIndices'][start:end]
+                                trace_past.close()
                             break
                     # if the subhalo isn't in the merger trees the past few snaps, move on
                     if i == 4:
-                        start = end = 0
-
-            IDs_past = tracers_past['group']['TracerIDs'][start:end]
-            indices_past = tracers_past['group']['TracerIndices'][start:end]
-
-            isin_now  = np.isin(tracer_IDs, IDs_past)
-            isin_past = np.isin(IDs_past, tracer_IDs)
-
+                        IDs_past = indices_past = np.ones(1, dtype=int) * -1
+                        break
+                        
             # reorder tracer_IDs and tracer_indices such that:
             # 1st: cold gas tracers found in both snapshots
             # 2nd: new cold gas tracers (not in the previous snapshot)
             # 3rd: no longer cold gas tracers (was in previous snapshot but no longer)
-
-            # find the indices of group 3
-            isin_group3    = np.isin(tracers['TracerID'], IDs_past[~isin_past])
-            IDs_group3     = tracers['TracerID'][isin_group3]
-            indices_group3 = np.where(isin_group3)[0]
-
-            IDs = np.concatenate([tracer_IDs[isin_now],
-                                  tracer_IDs[~isin_now],
-                                  IDs_group3])
-
-            indices = np.concatenate([tracer_indices[isin_now],
-                                      tracer_indices[~isin_now],
-                                      indices_group3])
-
-            # save the offset information
-            offsets_subhalo['SubhaloLength'][subfind_i]            = len(IDs)
-            offsets_subhalo['SubhaloLengthColdGas'][subfind_i]     = len(tracer_IDs)
-            offsets_subhalo['SubhaloLengthColdGas_new'][subfind_i] = len(tracer_IDs[~isin_now])
+            isin_now  = np.isin(tracer_IDs_sub, IDs_past)
+            isin_past = np.isin(IDs_past, tracer_IDs_sub)
+            
+            IDs_group1 = tracer_IDs_sub[isin_now]
+            IDs_group2 = tracer_IDs_sub[~isin_now]
+            IDs_group3 = IDs_past[~isin_past]
+            
+            indices_group1 = tracer_indices_sub[isin_now]
+            indices_group2 = tracer_indices_sub[~isin_now]
+            # find the indices of group3 tracers later
+            # the tracer indices vary with each snapshot, so we can't use the indices from the previous snaps
+            # save -1 now, such that we only need to find the indices once per snapshot (rather than per subhalo)
+            indices_group3 = np.ones(len(IDs_group3), dtype=int) * -1
+            
+            tracer_IDs_sub = np.concatenate([IDs_group1, IDs_group2, IDs_group3])
+            tracer_indices_sub = np.concatenate([indices_group1, indices_group2, indices_group3])
+            
+            # save the info in the offsets catalog
+            offsets_subhalo['SubhaloLength'][subfind_i] = len(tracer_IDs_sub) # all three groups
+            offsets_subhalo['SubhaloLengthColdGas'][subfind_i] = len(IDs_group1) + len(IDs_group2) # groups 1 and 2
+            offsets_subhalo['SubhaloLengthColdGas_new'][subfind_i] = len(IDs_group2) # group 2
+        
         # end cross match with previous snapshot(s)
 
         start       = offsets_subhalo['SubhaloOffset'][subfind_i]
         length      = offsets_subhalo['SubhaloLength'][subfind_i]
         length_cgas = offsets_subhalo['SubhaloLengthColdGas'][subfind_i]
-
+        
         # save the tracerIDs and indices
-        tracers_subhalo['TracerIDs'][start:start+length] = tracer_IDs[indices]
-        tracers_subhalo['TracerIndices'][start:start+length] = tracer_indices[indices]
+        tracers_subhalo['TracerIDs'][start:start+length] = tracer_IDs_sub
+        tracers_subhalo['TracerIndices'][start:start+length] = tracer_indices_sub
 
-        # find the appropriate particles indices
-        part_indices = particle_indices[indices]
+        # find the appropriate particles indices for groups 1, 2
+        part_indices = particle_indices[sub_indices]
         tracers_subhalo['ParentIndices'][start:start+length_cgas] = part_indices
         tracers_subhalo['ParentPartType'][start:start+length_cgas] = np.ones(len(part_indices), dtype=int) * gas_ptn
-        tracers_subhalo['ParentGasTemp'][start:start+length_cgas] = temperatures[indices]
+        tracers_subhalo['ParentGasTemp'][start:start+length_cgas] = temperatures[sub_indices]
 
         # for now, make the parent indices and parent part type -1
         # then later load the other baryonic particles in the sim and match
@@ -255,7 +271,7 @@ def match_tracers(snap):
             outf.close()
 
     return
-        
+
 
 def find_coldgascells(subfindIDs, snap):
     """
@@ -293,8 +309,13 @@ def find_coldgascells(subfindIDs, snap):
     tcoldgas = 10.**(4.5) # [K]
 
     for i, subfindID in enumerate(subfindIDs):
+
         if i > 0:
             offsets[i] = offsets[i-1] + lengths[i-1]
+            
+        if subfindID == -1:
+            continue
+            
         r = il.snapshot.getSnapOffsets(basePath, snap, subfindID, 'Subhalo')
         start = r['offsetType'][gas_ptn]
         end = r['lenType'][gas_ptn]
@@ -307,7 +328,7 @@ def find_coldgascells(subfindIDs, snap):
         # check if there are any bound gas cells
         if end == 0:
             continue
-
+            
         ParticleIDs[offsets[i]:offsets[i]+lengths[i]] = gas_cells['ParticleIDs'][start:start+end][indices]
         Particle_indices[offsets[i]:offsets[i]+lengths[i]] = start + np.where(indices)[0]
 
@@ -321,6 +342,7 @@ def find_coldgascells(subfindIDs, snap):
         print('Warning')
     
     return ParticleIDs, Particle_indices, Temperatures, offsets, lengths
+
 
 # from Dylan Nelson
 def match3(ar1, ar2, firstSorted=False, parallel=False):
@@ -740,6 +762,9 @@ def track_subfindIDs(subfindIDs, z0_flag=True):
         else:
             tree = ru.loadMainTreeBranch(snapNum, subfindID, sim=sim, treeName=treeName,
                                          fields=fields, min_snap=snapNum, max_snap=max_snap)
+            
+        if not tree:
+            continue
 
         # find at which snaps the subhalo was identified
         isin = np.isin(snaps, tree['SnapNum'])
