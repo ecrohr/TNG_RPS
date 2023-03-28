e546bf9a7f689feb11fbd528134aac2685634e65
Eric Rohr <rohr@mpia.de>
2023-03-28 15:46:19 +0200

    exectutes for TNG-Cluster

--- src/tng_rps/Run_Pipeline.py
+++ src/tng_rps/Run_Pipeline.py
@@ -9,29 +9,12 @@ import numpy as np
 import h5py
 import rohr_utils as ru
 
-global sim, basePath, Header, h
-global max_snap, min_snap, SnapNums, Times, BoxSizes
-global gas_ptn, dm_ptn, tracer_ptn, star_ptn, bh_ptn
+# import globals
+import globals
+globals.globals()
 
-sim = 'L680n8192TNG'
-basePath = ru.loadbasePath(sim)
+# create the indices
+from Create_SubfindIndices import run_subfindindices
+run_subfindindices()
 
-max_snap = 99
-min_snap = 0
 
-Header = il.groupcat.loadHeader(basePath, max_snap)
-h = Header['HubbleParam']
-
-SnapNums = range(max_snap, min_snap-1, -1)
-Times = np.zeros(len(SnapNums), dtype=float)
-BoxSizes = np.zeros(len(SnapNums), dtype=float)
-for i, SnapNum in enumerate(SnapNums):
-    header = il.groupcat.loadHeader(basePath, SnapNum)
-    Times[i] = header['Time']
-    BoxSizes[i] = header['BoxSize'] * Times[i] / h
-
-gas_ptn = il.util.partTypeNum('gas')
-dm_ptn = il.util.partTypeNum('dm')
-tracer_ptn = il.util.partTypeNum('tracer')
-star_ptn = il.util.partTypeNum('star')
-bh_ptn = il.util.partTypeNum('bh')
