11a87fd9aa8e98db925454fd9e10608f45a6d953
Eric Rohr <rohr@mpia.de>
2023-09-16 22:42:18 +0200

    plot updates.

--- src/tng_rps/Create_SubfindGasRadProf.py
+++ src/tng_rps/Create_SubfindGasRadProf.py
@@ -296,10 +296,12 @@ def return_subfindGRP(snapnum, subfindID, Config):
     hotgas_mass_shells = np.histogram(hotgas_radii, bins=radii_bins, weights=hotgas_masses)[0]
     gas_mass_shells = np.histogram(gas_radii, bins=radii_bins, weights=gas_masses)[0]
 
+    """
     # if the gas mass within the shell is 0, replace it with the low limit value
     coldgas_mass_shells[coldgas_mass_shells == 0] = gas_lolim
     hotgas_mass_shells[hotgas_mass_shells == 0] = gas_lolim
     gas_mass_shells[gas_mass_shells == 0] = 2.0 * gas_lolim
+    """
 
     coldgas_densities_shells = coldgas_mass_shells / vol_shells
     hotgas_densities_shells = hotgas_mass_shells / vol_shells
